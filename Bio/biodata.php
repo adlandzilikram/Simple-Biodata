7ed1f7b0ddfad256e6c0a64d8ca813d079df9387
adlandzilikram <55419265+adlandzilikram@users.noreply.github.com>
2020-09-18 10:20:52 +0700

    Biodata
    Bio

---
+++ Bio/biodata.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+	<head>
+				   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+   				   <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet">
+				   <title>Biodata</title>
+	</head>
+		<body style="background-color:#dedede;">
+		<br><br>
+	<div class="card mb-3" style="max-width: 640px;margin-left: auto; margin-right: auto;">
+  		<div >
+    		<div class="col-md-4">
+    			<br><br>
+      			<img src="1.jpg"  width="500" height="300" id="foto" style="margin-left: 50px">
+    		</div><br>
+    			<div class="col-md-8"  style=" max-width: 740px;margin-left: 80px";">
+      				<div class="card-body">
+      					<center>
+        					<h2 class="card-title" style="max-width: 640px;margin-left: auto; margin-right: 210px; font-family:inherit;">Biodata</h2>
+        				</center>
+        				<br>
+        				<table style="font-family: 'Lora', serif;">
+        				<tr class="card-text">
+        					<td>Nama</td>
+							<td> &nbsp;:&nbsp; <?php echo "Mochammad Adlan Dzil Ikram"; ?> </td>
+						</tr>
+						<tr class="card-text">
+        					<td>Nama panggilan</td>
+							<td> &nbsp;:&nbsp; <?php echo "Bojes"; ?> </td>
+						</tr>
+						<tr class="card-text">
+        					<td>No</td>
+							<td> &nbsp;:&nbsp; <?php echo "081908313916"; ?> </td>
+						</tr>
+						<tr class="card-text">
+        					<td>Tempat, Tanggal Lahir</td>
+							<td> &nbsp;:&nbsp; <?php echo "Bogor, 19 April 2002"; ?> </td>
+						</tr>
+						<tr class="card-text">
+        					<td>Cita cita</td>
+							<td> &nbsp;:&nbsp; <?php echo "Programmer & ahlul qur'an"; ?> </td>
+						</tr>
+						<tr class="card-text">
+        					<td>Jenis Kelamin</td>
+							<td> &nbsp;:&nbsp; <?php echo "Laki laki"; ?> </td>
+						</tr>
+						<tr class="card-text">
+        					<td>Hobi</td>
+							<td> &nbsp;:&nbsp; <?php echo "Olahraga"; ?> </td>
+						</tr>
+						<tr class="card-text">
+        					<td>Rombel</td>
+							<td> &nbsp;:&nbsp; <?php echo "RPL XII"; ?> </td>
+						</tr>
+						<tr class="card-text">
+        					<td>Rayon</td>
+							<td> &nbsp;:&nbsp; <?php echo "Wikrama 5"; ?> </td>
+						</tr>
+						<tr class="card-text">
+        					<td>Alamat</td>
+							<td> &nbsp;:&nbsp; <?php echo "Kp. Cikereteg ds. Ciderum RT 11 RW 02"; ?> </td>
+						</tr>
+						<tr class="card-text">
+        					<td>Facebook</td>
+							<td> &nbsp;:&nbsp; <?php echo "Adlan Dzil Ikram"; ?> </td>
+						</tr>
+						<tr class="card-text">
+        					<td>Instagram</td>
+							<td> &nbsp;:&nbsp; <?php echo "ikram_dzil19"; ?> </td>
+						</tr>
+						<tr class="card-text">
+        					<td>twitter</td>
+							<td> &nbsp;:&nbsp; <?php echo "ikramdzil19"; ?> </td>
+						</tr>
+					</table>					
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</html>
